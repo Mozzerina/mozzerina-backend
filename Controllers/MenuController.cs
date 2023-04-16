20f881c39b3d12a0572966f6a14338bb95242ca8
Dima <dmitrydatsko.of@gmail.com>
2023-04-16 18:47:01 +0300

    BackendDeveloper

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -16,26 +16,45 @@ namespace Mozzerina.Controllers
         {
             _dataContext = dataContext;
         }
+        //[HttpGet]
+        //public async Task<IActionResult> GetMenu()
+        //{
+        //    var drink = await _dataContext.Drinks.ToListAsync();
+        //    var food = await _dataContext.Foods.ToListAsync();
+        //    var athomecoffee = await _dataContext.AtHomeCoffees.ToListAsync();
+        //    var merchandise = await _dataContext.Merchandises.ToListAsync();
+        //    var giftcard = await _dataContext.GiftCards.ToListAsync();
+        //    var d = new Dictionary<string, List<Drink>> { { "Drinks", drink } };
+        //    var f = new Dictionary<string, List<Food>> { { "Food", food } };
+        //    var a = new Dictionary<string, List<AtHomeCoffee>> { { "At Home Coffee", athomecoffee } };
+        //    var m = new Dictionary<string, List<Merchandise>> { { "Merchandise", merchandise } };
+        //    var g = new Dictionary<string, List<GiftCard>> { { "Gift Card", giftcard } };
+
+        //    List<object> obj = new()
+        //    {
+        //        d, f, a, m, g
+        //    };
+
+        //    return Ok(obj);
+        //}
         [HttpGet]
         public async Task<IActionResult> GetMenu()
         {
             var drink = await _dataContext.Drinks.ToListAsync();
             var food = await _dataContext.Foods.ToListAsync();
-            var athomecoffee = await _dataContext.AtHomeCoffees.ToListAsync();
-            var merchandise = await _dataContext.Merchandises.ToListAsync();
-            var giftcard = await _dataContext.GiftCards.ToListAsync();
-            var d = new Dictionary<string, List<Drink>> { { "Drinks", drink } };
-            var f = new Dictionary<string, List<Food>> { { "Food", food } };
-            var a = new Dictionary<string, List<AtHomeCoffee>> { { "At Home Coffee", athomecoffee } };
-            var m = new Dictionary<string, List<Merchandise>> { { "Merchandise", merchandise } };
-            var g = new Dictionary<string, List<GiftCard>> { { "Gift Card", giftcard } };
-
-            List<object> obj = new()
+            var athome = await _dataContext.AtHomeCoffees.ToListAsync();
+            var merc = await _dataContext.Merchandises.ToListAsync();
+            var gift = await _dataContext.GiftCards.ToListAsync();
+            List<MenuDto> menu = new()
             {
-                d, f, a, m, g
+                new MenuDto {Name = "Drinks", Items = drink.ToList<object>()},
+                new MenuDto {Name = "Food", Items = food.ToList<object>()},
+                new MenuDto {Name = "At Home Coffee", Items = athome.ToList<object>()},
+                new MenuDto {Name = "Merchandise", Items = merc.ToList<object>()},
+                new MenuDto {Name = "Gift Cards", Items = gift.ToList<object>()},
             };
 
-            return Ok(obj);
+            return Ok(menu);
         }
     }
 }
--- Data/DTO/MenuDto.cs
+++ Data/DTO/MenuDto.cs
@@ -1,14 +1,10 @@
 ﻿using Mozzerina.Models;
-using Newtonsoft.Json;
 
 namespace Mozzerina.Data.DTO
 {
     public class MenuDto
     {
-        public Dictionary<string, List<Drink>> Drink { get; set; }
-        public Dictionary<string, List<Food>> Food { get; set; }
-        public Dictionary<string, List<AtHomeCoffee>> AtHomeCoffee { get; set; }
-        public Dictionary<string, List<Merchandise>> Merchandise { get; set; }
-        public Dictionary<string, List<GiftCard>> GiftCard { get; set; }
+        public string Name { get; set; } = "Drinks";
+        public List<object> Items { get; set; }
     }
 }
