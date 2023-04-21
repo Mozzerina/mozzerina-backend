f7749a2048d6ea3728cf032f6779ead7fda7386a
Dima <dmitrydatsko.of@gmail.com>
2023-04-21 20:40:12 +0300

    GetProductChange

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -20,9 +20,31 @@ namespace Mozzerina.Controllers
         [HttpGet]
         public async Task<IActionResult> GetProduct(int id)
         {
-            var product = await _dataContext.Products.FirstOrDefaultAsync(p => p.SubProductId == id);
-            
-            if(product == null)
+            var product = await _dataContext.Products
+                .Include(p => p.SubProduct.ProductType.MenuType)
+                .Where(p => p.SubProductId == id)
+                .Select(p => new
+                {
+                    product = new
+                    {
+                        p.Id,
+                        p.Name,
+                        p.Image,
+                        p.Description,
+                        p.Callories,
+                        p.Short,
+                        p.Tall,
+                        p.Grande,
+                        p.Venti,
+                        p.Ingredients,
+                        p.Allergens,
+                        p.Nutrition,
+                    },
+                    sizeOpts = p.SubProduct.ProductType.MenuType.Id < 8
+                })
+                .FirstOrDefaultAsync();
+
+            if (product == null)
             {
                 return NotFound("Product not found");
             }
